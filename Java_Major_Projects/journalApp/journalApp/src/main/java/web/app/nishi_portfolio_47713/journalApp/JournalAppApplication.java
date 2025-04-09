3257a33cfed001cbad7fd9dad96a56ddcac3fc9b
Nishi Tiwari <nishit5195@gmail.com>
2025-04-09 11:18:22 +0530

    Major Project

---
+++ Java_Major_Projects/journalApp/journalApp/src/main/java/web/app/nishi_portfolio_47713/journalApp/JournalAppApplication.java
@@ -0,0 +1,13 @@
+package web.app.nishi_portfolio_47713.journalApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JournalAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JournalAppApplication.class, args);
+	}
+
+}
---
+++ Java_Major_Projects/journalApp/journalApp/src/test/java/web/app/nishi_portfolio_47713/journalApp/JournalAppApplicationTests.java
@@ -0,0 +1,13 @@
+package web.app.nishi_portfolio_47713.journalApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JournalAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
